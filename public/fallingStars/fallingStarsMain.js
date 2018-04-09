3fa10505f292f72cffeeed0fc043625b3f19005f
Alex <doll3098@ou.edu>
2018-04-09 08:02:34 +0000

    initial commit

---
+++ public/fallingStars/fallingStarsMain.js
@@ -0,0 +1,35 @@
+var Phaser;
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: {
+                y: 300
+            },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image("sky", "fallingStarsAssets/sky.png");
+};
+
+function create() {
+    this.add.image(400, 300, "sky");
+};
+
+function update() {
+    
+};
